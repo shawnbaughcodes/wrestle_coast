64a0108f6051acbddb5272ca936f66b3e71d1db4
Shawn Baugh II <shawnbaughcreative@gmail.com>
2017-04-26 19:22:35 -0500

    Added registration and logging in methods to the views.py file. Added validation to the views which will refer to the models.py. Added User table to the models.py file. Will add validation to the file tomorrow. Created 2 additional html files for login and registration pages. Added buttons and forms to each page.

--- wrestle_coast/models.py
+++ wrestle_coast/models.py
@@ -3,3 +3,10 @@ from __future__ import unicode_literals
 from django.db import models
 import re, bcrypt
 # Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
--- wrestle_coast/views.py
+++ wrestle_coast/views.py
@@ -4,9 +4,33 @@ from django.contrib import messages
 # Create your views here.
 def index(request):
     return render(request, 'wrestle_coast/index.html')
-
+# END INDEX METHOD
+# START REGISTER RENDER
 def register(request):
     return render(request, 'wrestle_coast/register.html')
-
+# START REGISTRATION
+def registration(request):
+    is_valid = User.objects.register_validate(request.POST)
+    if type(is_valid) == dict:
+        for error in is_valid['errors']:
+            messages.error(request, error)
+            return redirect('/register')
+    else:
+        user = User.objects.create_user(request.POST)
+        request.session['user_id'] = user.id
+        return redirect('/home')
+# END ALL REGISTER FUNCTIONS******
+# START LOGIN RENDER
 def login(request):
     return render(request, 'wrestle_coast/login.html')
+# START LOGGING IN
+def logging_in(request):
+    is_valid = User.objects.login_validate(request.POST)
+    if is_valid['status'] == True:
+        request.session['user_id'] = is_valid['user'].id
+        return redirect('/home')
+    else:
+        if is_valid['status'] == False:
+            messages.error(request, is_valid['message'])
+        return redirect('/login')
+# END ALL LOGGING FUNCTIONS*********
